18e5d7913edd43fd41e69544ad35a907ef2ba963
nimabht <nimabeheshtaein99@gmail.com>
2023-02-27 17:23:53 +0330

    not found handler fixed.

--- 1-2/middlewares/notFoundHandler.js
+++ 1-2/middlewares/notFoundHandler.js
@@ -1,9 +1,10 @@
+const { messageHtmlGenerator } = require("../tools/htmlGenerators");
 function notFoundHandler(req, res, next) {
-  res.status(404);
-  res.render("sendMessage", {
+  const html = messageHtmlGenerator({
     title: "Page Not Found",
     message: "The page you are looking for could not be found :(",
   });
+  res.status(404).send(html);
 }
 
 module.exports = notFoundHandler;
